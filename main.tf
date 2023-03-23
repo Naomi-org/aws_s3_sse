725b3c0540f9a3e4b2535940660450f1e98b7f4d
Tekangbush <careeremt9@gmail.com>
2023-03-23 18:55:18 +0000

    creating an instance

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "my_instance" {
+  ami                     = "ami-0557a15b87f6559cf"
+  instance_type           = "t2.micro"
+  tags ={
+    Name ="ec2_instance"
+}
+}
+
+provider "aws" {
+  region     = "us-west-1"
+ 
+}
